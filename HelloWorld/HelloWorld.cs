4450772f52bd1025940663e8e8bdf91b0a87a868
pphatdev <leatsophat02@gmail.com>
2024-03-09 19:11:16 +0700

    C Sharp Hello World

---
+++ HelloWorld/HelloWorld.cs
@@ -0,0 +1,17 @@
+﻿
+internal class HelloWorld
+{
+    static void Main(string[] args)
+    {
+        if (args is null)
+        {
+            throw new ArgumentNullException(nameof(args));
+        }
+
+        int a = 10;
+        int b = 20;
+        int c;
+        c = a + b;
+        Console.WriteLine("HelloWorld! a:10 + b:20 = c:" + c);
+    }
+}
